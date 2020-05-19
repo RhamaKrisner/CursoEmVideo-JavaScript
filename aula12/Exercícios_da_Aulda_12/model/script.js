ffb17776faa8dd567dda200b2815e208e68345f7
Rhama Krisner <rhama.krisner@hotmail.com>
2020-05-19 03:04:14 -0300

    Update Mai-19
    Site da aula 12 ex012 Curso em Vídeo

--- aula12/Exercícios_da_Aulda_12/model/script.js
+++ aula12/Exercícios_da_Aulda_12/model/script.js
@@ -1,10 +1,21 @@
 function carregar(){
-
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours
+   var hora = 18
     msg.innerHTML = `Agora são ${hora} horas.`
-
+    if (hora >=4 && hora < 12){
+        //bom dia
+        img.src = 'img/manha.png'
+        document.body.style.background = '#CB7A78'
+    }else if(hora >= 12 && hora < 18){
+        //boa tarde
+        document.body.style.background = '#822a04'
+        img.src = 'img/tarde.png'
+    }else{
+        //boa noite
+        img.src = 'img/noite.png'
+        document.body.style.background ='#2b2b2b'
+    }
 
 }
