e91de0d7fd39dc1b991badfcdf63f2dd9fa3ae49
Rhama Krisner <rhama.krisner@hotmail.com>
2020-05-21 19:32:37 -0300

    Update

--- aula12/ex015/js/script.js
+++ aula12/ex015/js/script.js
@@ -2,18 +2,24 @@ function verificar(){
     var data = new Date()   
     var ano = data.getFullYear()
     var formAno = document.getElementById('ano')
-    var res = document.querySelector('div#res')
+    var res = document.getElementById('res')
     if(formAno.value.length == 0 || Number(formAno.value) > ano || formAno.value < formAno - 120)    {
         window.alert('Verifique os dados e tente novamente!')
     }else{
-        var fsex = document.getElementsByName('radsex')[0]
-        var idade = ano - formAno.value
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(formAno.value)
+
         var genero = ''
-        if(fsex[0].checked){
+        if (fsex[0].checked){
             genero = 'Homem'
-        }else if (fsex[1].checked){
+        }else if(fsex[1].checked){
             genero = 'Mulher'
         }
-        res.innerHTML = `${genero} com ${idade} anos.`
+        res.style.textAlign = 'center'
+        res.innerHTML = `${genero}, com ${idade} anos.`
+        
     }
 }
+
+
+    
