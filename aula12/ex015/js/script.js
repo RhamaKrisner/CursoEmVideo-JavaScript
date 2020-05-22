958619ab4874e42350c317c6551036a65890e9f4
Rhama Krisner <rhama.krisner@hotmail.com>
2020-05-21 23:51:05 -0300

    Site Identificador de Idade Mai-21

--- aula12/ex015/js/script.js
+++ aula12/ex015/js/script.js
@@ -3,7 +3,7 @@ function verificar(){
     var ano = data.getFullYear()
     var formAno = document.getElementById('ano')
     var res = document.getElementById('res')
-    if(formAno.value.length == 0 || Number(formAno.value) > ano || formAno.value < formAno - 120)    {
+    if(formAno.value.length == 0 || Number(formAno.value) > ano)    {
         window.alert('Verifique os dados e tente novamente!')
     }else{
         var fsex = document.getElementsByName('radsex')
@@ -14,29 +14,41 @@ function verificar(){
         img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             genero = 'Homem'
-            if(idade >= 0 && < 10){
+            if(idade >= 0 && idade < 10){
                 //criança
+                img.setAttribute('src', 'img/Bebe-H.png')
             }else if(idade < 21){
                 //Jovem
+                img.setAttribute('src', 'img/Jovem-H.png')
             }else if(idade < 50){
                 //adulto
+                img.setAttribute('src', 'img/Adulto-H.png')
             }else{
                 //idoso
+                img.setAttribute('src', 'img/Idoso-H.png')
             }
         }else if(fsex[1].checked){
             genero = 'Mulher'
             if(idade >= 0 && idade < 10){
                 //criança
+                img.setAttribute('src', 'img/Bebe-M.png')
             }else if(idade < 21){
                 //jovem
+                img.setAttribute('scr', 'img/Jovem-M.png')
             }else if(idade < 50){
                 //adulto
+                img.setAttribute('src', 'img/Adulto-M.png')
             }else{
                 //idoso
+                img.setAttribute('src', 'img/Idoso-M.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `${genero}, com ${idade} anos.`
+        res.appendChild(img)
+        img.width = 300
+        img.height = 300
+        img.style.textAlign = 'center'
         
     }
 }
