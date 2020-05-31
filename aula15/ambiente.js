ad963d7431e86cbd2d855b8c50c7439aeeacbb52
Rhama Krisner <rhama.krisner@hotmail.com>
2020-05-30 23:35:30 -0300

    Update

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,9 +1,14 @@
-let num = [5, 8, 2, 9, 3]
+let num = [5, 8, 2, 9, 3,]
+
 num.push()
 num.sort()
-console.log(`Quantidade de posições ${num.length}`)
-let pos = num
-for(let pos =0 ; pos <= num.length; pos++  ){
-    console.log(num[pos])
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O valor do primeiro vetor é ${num[0]}`)
+let pos = num.indexOf(3)
+if(pos == -1){
+    console.log(`Valor não encontrado`)
+}else{
+console.log(`O valor está na posição ${pos}`)
 }
  
