2cdf91206b517c9788c1d6d168f3a733d65f073c
Rhama Krisner <rhama.krisner@hotmail.com>
2020-05-31 20:26:33 -0300

    Update

---
+++ aula16ex/js/script.js
@@ -0,0 +1,10 @@
+function adicionar(){
+    let num = document.getElementById('num')
+    let tab = document.getElementById('adicionar')
+    if(num.nodeValue.length == 0){
+        window.alert('Número não identificado. Adicione um valor')
+    }else{
+        let numero = []
+           
+    }
+}
