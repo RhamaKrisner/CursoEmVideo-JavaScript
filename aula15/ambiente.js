e37054382f4a2a84bae6e127904fe5cb16b381c0
Rhama Krisner <rhama.krisner@hotmail.com>
2020-05-30 21:16:09 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3]
+num.push()
+num.sort()
+console.log(`Quantidade de posições ${num.length}`)
+let pos = num
+for(let pos =0 ; pos <= num.length; pos++  ){
+    console.log(num[pos])
+}
+ 
